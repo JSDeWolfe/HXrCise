a7471116b100244bef0e35058615d6c30d172a8a
James <james@trueface.ai>
2019-01-12 12:16:11 -0800

    entity update

--- app/src/main/java/com/type12clarity/hxrcise/hxrcise/Entities/SongList.java
+++ app/src/main/java/com/type12clarity/hxrcise/hxrcise/Entities/SongList.java
@@ -1,4 +1,37 @@
 package com.type12clarity.hxrcise.hxrcise.Entities;
 
 public class SongList {
+    private Long _slid;
+    private String _title;
+    private String _datetime;
+
+    public SongList() {
+    }
+
+    public SongList(String title) {
+        this._title = title;
+    }
+
+    public void set_id(Long _id) {
+        this._slid = _id;
+    }
+
+    public Long get_woid() {
+        return _slid;
+    }
+
+    public void set_word(String title) {
+        this._title = title;
+    }
+
+    public String get_title() {
+        return _title;
+    }
+
+    public void set_datetime(String datetime) {
+        this._datetime = datetime;
+    }
+
+    public String get_datetime() { return _datetime; }
 }
+
